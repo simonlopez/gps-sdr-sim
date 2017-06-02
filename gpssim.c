2eace2a9d70e8726245d1df18e7ecb1fa04ac56f
Simon Lopez <simon.lopez@slopez.org>
2017-06-02 13:26:03 +0200

    allow output to stdin

--- gpssim.c
+++ gpssim.c
@@ -1514,7 +1514,7 @@ int generateNavMsg(gpstime_t g, channel_t *chan, int init)
 		// Sanity check
 		if (((chan->dwrd[1])&(0x1FFFFUL<<13)) != ((tow&0x1FFFFUL)<<13))
 		{
-			printf("\nWARNING: Invalid TOW in subframe 5.\n");
+			fprintf(log,"\nWARNING: Invalid TOW in subframe 5.\n");
 			return(0);
 		}
 		*/
@@ -1648,7 +1648,7 @@ int allocateChannel(channel_t *chan, ephem_t *eph, ionoutc_t ionoutc, gpstime_t 
 
 void usage(void)
 {
-	printf("Usage: gps-sdr-sim [options]\n"
+	fprintf(stdout,"Usage: gps-sdr-sim [options]\n"
 		"Options:\n"
 		"  -e <gps_nav>     RINEX navigation file for GPS ephemerides (required)\n"
 		"  -u <user_motion> User motion file (dynamic mode)\n"
@@ -1670,6 +1670,8 @@ void usage(void)
 int main(int argc, char *argv[])
 {
 	clock_t tstart,tend;
+
+	FILE *log = stdout;
 
 	FILE *fp;
 
@@ -1775,12 +1777,16 @@ int main(int argc, char *argv[])
 			break;
 		case 'o':
 			strcpy(outfile, optarg);
+			if(strcmp(outfile, "-")==0)
+			{
+				log = stderr;
+			}
 			break;
 		case 's':
 			samp_freq = atof(optarg);
 			if (samp_freq<1.0e6)
 			{
-				printf("ERROR: Invalid sampling frequency.\n");
+				fprintf(stderr,"ERROR: Invalid sampling frequency.\n");
 				exit(1);
 			}
 			break;
@@ -1788,7 +1794,7 @@ int main(int argc, char *argv[])
 			data_format = atoi(optarg);
 			if (data_format!=SC01 && data_format!=SC08 && data_format!=SC16)
 			{
-				printf("ERROR: Invalid I/Q data format.\n");
+				fprintf(stderr,"ERROR: Invalid I/Q data format.\n");
 				exit(1);
 			}
 			break;
@@ -1818,7 +1824,7 @@ int main(int argc, char *argv[])
 			if (t0.y<=1980 || t0.m<1 || t0.m>12 || t0.d<1 || t0.d>31 ||
 				t0.hh<0 || t0.hh>23 || t0.mm<0 || t0.mm>59 || t0.sec<0.0 || t0.sec>=60.0)
 			{
-				printf("ERROR: Invalid date and time.\n");
+				fprintf(stderr,"ERROR: Invalid date and time.\n");
 				exit(1);
 			}
 			t0.sec = floor(t0.sec);
@@ -1828,7 +1834,7 @@ int main(int argc, char *argv[])
 			duration = atof(optarg);
                         if (duration<0.0)
                         {
-                                printf("ERROR: Invalid duration.\n");
+                                fprintf(stderr,"ERROR: Invalid duration.\n");
                                 exit(1);
                         }
                         if(duration>((double)USER_MOTION_SIZE/10.0))
@@ -1853,7 +1859,7 @@ int main(int argc, char *argv[])
 
 	if (navfile[0]==0)
 	{
-		printf("ERROR: GPS ephemeris file is not specified.\n");
+		fprintf(stderr,"ERROR: GPS ephemeris file is not specified.\n");
 		exit(1);
 	}
 
@@ -1890,12 +1896,12 @@ int main(int argc, char *argv[])
 
 		if (numd==-1)
 		{
-			printf("ERROR: Failed to open user motion / NMEA GGA file.\n");
+			fprintf(stderr,"ERROR: Failed to open user motion / NMEA GGA file.\n");
 			exit(1);
 		}
 		else if (numd==0)
 		{
-			printf("ERROR: Failed to read user motion / NMEA GGA data.\n");
+			fprintf(stderr,"ERROR: Failed to read user motion / NMEA GGA data.\n");
 			exit(1);
 		}
 
@@ -1907,14 +1913,14 @@ int main(int argc, char *argv[])
 	{ 
 		// Static geodetic coordinates input mode: "-l"
 		// Added by scateu@gmail.com 
-		printf("Using static location mode.\n");
+		fprintf(log,"Using static location mode.\n");
 		llh2xyz(llh,xyz[0]); // Convert llh to xyz
 
 		numd = iduration;
 	}
 /*
-	printf("xyz = %11.1f, %11.1f, %11.1f\n", xyz[0][0], xyz[0][1], xyz[0][2]);
-	printf("llh = %11.6f, %11.6f, %11.1f\n", llh[0]*R2D, llh[1]*R2D, llh[2]);
+	fprintf(log,"xyz = %11.1f, %11.1f, %11.1f\n", xyz[0][0], xyz[0][1], xyz[0][2]);
+	fprintf(log,"llh = %11.6f, %11.6f, %11.1f\n", llh[0]*R2D, llh[1]*R2D, llh[2]);
 */
 	////////////////////////////////////////////////////////////
 	// Read ephemeris
@@ -1924,19 +1930,19 @@ int main(int argc, char *argv[])
 
 	if (neph==0)
 	{
-		printf("ERROR: No ephemeris available.\n");
+		fprintf(stderr,"ERROR: No ephemeris available.\n");
 		exit(1);
 	}
 
 	if ((verb==TRUE)&&(ionoutc.vflg==TRUE))
 	{
-		printf("  %12.3e %12.3e %12.3e %12.3e\n", 
+		fprintf(log,"  %12.3e %12.3e %12.3e %12.3e\n", 
 			ionoutc.alpha0, ionoutc.alpha1, ionoutc.alpha2, ionoutc.alpha3);
-		printf("  %12.3e %12.3e %12.3e %12.3e\n", 
+		fprintf(log,"  %12.3e %12.3e %12.3e %12.3e\n", 
 			ionoutc.beta0, ionoutc.beta1, ionoutc.beta2, ionoutc.beta3);
-		printf("   %19.11e %19.11e  %9d %9d\n",
+		fprintf(log,"   %19.11e %19.11e  %9d %9d\n",
 			ionoutc.A0, ionoutc.A1, ionoutc.tot, ionoutc.wnt);
-		printf("%6d\n", ionoutc.dtls);
+		fprintf(log,"%6d\n", ionoutc.dtls);
 	}
 
 	for (sv=0; sv<MAX_SAT; sv++) 
@@ -2001,11 +2007,11 @@ int main(int argc, char *argv[])
 		{
 			if (subGpsTime(g0, gmin)<0.0 || subGpsTime(gmax, g0)<0.0)
 			{
-				printf("ERROR: Invalid start time.\n");
-				printf("tmin = %4d/%02d/%02d,%02d:%02d:%02.0f (%d:%.0f)\n", 
+				fprintf(stderr,"ERROR: Invalid start time.\n");
+				fprintf(stderr,"tmin = %4d/%02d/%02d,%02d:%02d:%02.0f (%d:%.0f)\n", 
 					tmin.y, tmin.m, tmin.d, tmin.hh, tmin.mm, tmin.sec,
 					gmin.week, gmin.sec);
-				printf("tmax = %4d/%02d/%02d,%02d:%02d:%02.0f (%d:%.0f)\n", 
+				fprintf(stderr,"tmax = %4d/%02d/%02d,%02d:%02d:%02.0f (%d:%.0f)\n", 
 					tmax.y, tmax.m, tmax.d, tmax.hh, tmax.mm, tmax.sec,
 					gmax.week, gmax.sec);
 				exit(1);
@@ -2018,9 +2024,9 @@ int main(int argc, char *argv[])
 		t0 = tmin;
 	}
 
-	printf("Start time = %4d/%02d/%02d,%02d:%02d:%02.0f (%d:%.0f)\n", 
+	fprintf(log,"Start time = %4d/%02d/%02d,%02d:%02d:%02.0f (%d:%.0f)\n", 
 		t0.y, t0.m, t0.d, t0.hh, t0.mm, t0.sec, g0.week, g0.sec);
-	printf("Duration = %.1f [sec]\n", ((double)numd)/10.0);
+	fprintf(log,"Duration = %.1f [sec]\n", ((double)numd)/10.0);
 
 	// Select the current set of ephemerides
 	ieph = -1;
@@ -2046,7 +2052,7 @@ int main(int argc, char *argv[])
 
 	if (ieph == -1)
 	{
-		printf("ERROR: No current set of ephemerides has been found.\n");
+		fprintf(stderr,"ERROR: No current set of ephemerides has been found.\n");
 		exit(1);
 	}
 
@@ -2059,7 +2065,7 @@ int main(int argc, char *argv[])
 
 	if (iq_buff==NULL)
 	{
-		printf("ERROR: Faild to allocate 16-bit I/Q buffer.\n");
+		fprintf(stderr,"ERROR: Faild to allocate 16-bit I/Q buffer.\n");
 		exit(1);
 	}
 
@@ -2068,7 +2074,7 @@ int main(int argc, char *argv[])
 		iq8_buff = calloc(2*iq_buff_size, 1);
 		if (iq8_buff==NULL)
 		{
-			printf("ERROR: Faild to allocate 8-bit I/Q buffer.\n");
+			fprintf(stderr,"ERROR: Faild to allocate 8-bit I/Q buffer.\n");
 			exit(1);
 		}
 	}
@@ -2077,16 +2083,20 @@ int main(int argc, char *argv[])
 		iq8_buff = calloc(iq_buff_size/4, 1); // byte = {I0, Q0, I1, Q1, I2, Q2, I3, Q3}
 		if (iq8_buff==NULL)
 		{
-			printf("ERROR: Faild to allocate compressed 1-bit I/Q buffer.\n");
+			fprintf(stderr,"ERROR: Faild to allocate compressed 1-bit I/Q buffer.\n");
 			exit(1);
 		}
 	}
 
-	// Open output file
-	if (NULL==(fp=fopen(outfile,"wb")))
-	{
-		printf("ERROR: Failed to open output file.\n");
-		exit(1);
+	if(strcmp(outfile, "-") == 0) {
+		fp = stdout;
+	} else {
+		// Open output file
+		if (NULL==(fp=fopen(outfile,"wb")))
+		{
+			fprintf(stderr,"ERROR: Failed to open output file.\n");
+			exit(1);
+		}
 	}
 
 	////////////////////////////////////////////////////////////
@@ -2110,7 +2120,7 @@ int main(int argc, char *argv[])
 	for(i=0; i<MAX_CHAN; i++)
 	{
 		if (chan[i].prn>0)
-			printf("%02d %6.1f %5.1f %11.1f %5.1f\n", chan[i].prn, 
+			fprintf(log,"%02d %6.1f %5.1f %11.1f %5.1f\n", chan[i].prn, 
 				chan[i].azel[0]*R2D, chan[i].azel[1]*R2D, chan[i].rho0.d, chan[i].rho0.iono_delay);
 	}
 
@@ -2203,7 +2213,7 @@ int main(int argc, char *argv[])
 								chan[i].iword++;
 								/*
 								if (chan[i].iword>=N_DWRD)
-									printf("\nWARNING: Subframe word buffer overflow.\n");
+									fprintf(log,"\nWARNING: Subframe word buffer overflow.\n");
 								*/
 							}
 
@@ -2300,11 +2310,11 @@ int main(int argc, char *argv[])
 			// Show ditails about simulated channels
 			if (verb==TRUE)
 			{
-				printf("\n");
+				fprintf(log,"\n");
 				for (i=0; i<MAX_CHAN; i++)
 				{
 					if (chan[i].prn>0)
-						printf("%02d %6.1f %5.1f %11.1f %5.1f\n", chan[i].prn,
+						fprintf(log,"%02d %6.1f %5.1f %11.1f %5.1f\n", chan[i].prn,
 							chan[i].azel[0]*R2D, chan[i].azel[1]*R2D, chan[i].rho0.d, chan[i].rho0.iono_delay);
 				}
 			}
@@ -2316,14 +2326,14 @@ int main(int argc, char *argv[])
 		// Update time counter
                 if(iumd%100==0)
                 {
-		        printf("\rTime into run = %4.1f", subGpsTime(grx, g0));
-		        fflush(stdout);
+		        fprintf(log,"\rTime into run = %4.1f", subGpsTime(grx, g0));
+		        fflush(log);
                 }
 	}
 
 	tend = clock();
 
-	printf("\nDone!\n");
+	fprintf(log,"\nDone!\n");
 
 	// Free I/Q buffer
 	free(iq_buff);
@@ -2332,7 +2342,7 @@ int main(int argc, char *argv[])
 	fclose(fp);
 
 	// Process time
-	printf("Process time = %.1f [sec]\n", (double)(tend-tstart)/CLOCKS_PER_SEC);
+	fprintf(log,"Process time = %.1f [sec]\n", (double)(tend-tstart)/CLOCKS_PER_SEC);
 
 	return(0);
 }
